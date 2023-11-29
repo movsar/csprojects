412298c9255202898ccc07c54f498a72d9956369
= <movsar.dev@gmail.com>
2023-11-29 20:50:43 +0300

    save

---
+++ Libraries/ConsoleUtils/InputService.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleUtils
+{
+    public class InputService
+    {
+        public static int? SelectMode(string[] modeDescriptions)
+        {
+            Console.WriteLine("Выберите режим: ");
+
+            foreach (var modeDescription in modeDescriptions)
+            {
+                Console.WriteLine(modeDescription);
+            }
+
+            try
+            {
+                // Пытаемся получить числовое значение
+                int? mode = Convert.ToInt32(Console.ReadLine());
+
+                if (mode < 0 || mode > modeDescriptions.Length)
+                {
+                    // Если ошибка, запускаем текущий метод, заново
+                    return SelectMode(modeDescriptions);
+                }
+
+                // Если код дошел сюда, возвращаем значение выбранного режима
+                return mode;
+            }
+            catch (Exception)
+            {
+                // Если ошибка, запускаем текущий метод, заново
+                return SelectMode(modeDescriptions);
+            }
+        }
+    }
+}
---
+++ OOP/Cakes/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace Cakes
+{
+    internal class Cake
+    {
+        public string Title { get; }
+        public string Ingredients { get; }
+        public decimal Price { get; set; }
+        public Cake(string title, string ingredients)
+        {
+            Title = title;
+            Ingredients = ingredients;
+        }
+    }
+}
