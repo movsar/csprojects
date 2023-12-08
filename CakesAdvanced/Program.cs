8d81d1be1f9c6d15c3b53e32a899bef7235fd465
Муслим Марашев <appl95@bk.ru>
2023-12-08 17:38:37 +0300

    #2

---
+++ CakesAdvanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CakesAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
