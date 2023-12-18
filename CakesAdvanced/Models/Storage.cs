9b960fe6aef3e900bb4a1c83a4c62afe4666c872
Jockey195 <136336256+Jockey195@users.noreply.github.com>
2023-12-19 00:43:47 +0300

    мои глаза выдали критическую ошибку

--- CakesAdvanced/Models/Storage.cs
+++ CakesAdvanced/Models/Storage.cs
@@ -3,11 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CakesAdvanced.Models
 {
     internal class Storage
     {
         private List<Ingredient> _allingredients = new List<Ingredient>();
+
+        public void SaveIngredients()
+        {
+
+        }
+
+        public void LoadIngredients()
+        {
+
+        }
+
+        public Storage()
+        {
+            LoadIngredients();
+        }
+
+        public Ingredient? FindIngredientByName(string Name)
+        {
+            return _allingredients.Find(x => x.Name.ToLower() == Name.ToLower());
+        }
+
+        public Ingredient GetIngredientByName(string Name)
+        {
+            return _allingredients.First(x => x.Name.ToLower() == Name.ToLower());  
+        }
+
+        public void AddIngredient(Ingredient ingredient)
+        {
+
+        }
+
+        public void AddIngredients(List<Ingredient> ingredients)
+        {
+
+        }
     }
 }
