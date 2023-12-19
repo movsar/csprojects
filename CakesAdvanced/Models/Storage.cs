6db5a61a43823f56239746e9e820a6ab4b6240c3
Jockey195 <136336256+Jockey195@users.noreply.github.com>
2023-12-19 22:58:30 +0300

    ()()()()()()()()

--- CakesAdvanced/Models/Storage.cs
+++ CakesAdvanced/Models/Storage.cs
@@ -9,18 +9,28 @@ namespace CakesAdvanced.Models
 {
     internal class Storage
     {
+        const string INGREDIENTS_PATH = "ingredients.json";
         private List<Ingredient> _allingredients = new List<Ingredient>();
 
         public void SaveIngredients()
         {
-
+            var serializationIngredients = JsonConvert.SerializeObject(_allingredients);
+            File.WriteAllText(INGREDIENTS_PATH, serializationIngredients);
         }
 
         public void LoadIngredients()
         {
-
+            if (File.Exists(INGREDIENTS_PATH))
+            {
+                var serializationIngredients = File.ReadAllText(INGREDIENTS_PATH);
+                _allingredients = JsonConvert.DeserializeObject<List<Ingredient>>(serializationIngredients);
+                return;
+            }
+            else
+            {
+                throw new Exception("Файл не найден!");
+            }
         }
-
         public Storage()
         {
             LoadIngredients();
@@ -28,12 +38,19 @@ namespace CakesAdvanced.Models
 
         public Ingredient? FindIngredientByName(string Name)
         {
-            return _allingredients.Find(x => x.Name.ToLower() == Name.ToLower());
+            try
+            {
+                return _allingredients.Find(x => x.Name.ToLower() == Name.ToLower());
+            }
+            catch
+            {
+                throw new Exception("Ингредиент не найден");
+            }
         }
 
         public Ingredient GetIngredientByName(string Name)
         {
-            return _allingredients.First(x => x.Name.ToLower() == Name.ToLower());  
+            return _allingredients.First(x => x.Name.ToLower() == Name.ToLower());
         }
 
         public void AddIngredient(Ingredient ingredient)
