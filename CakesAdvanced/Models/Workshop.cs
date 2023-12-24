4617cbb7827610cbd84ea429862a6c32c4122e26
Khamz T <djigit_195@mail.ru>
2023-12-24 23:49:53 +0300

    CakesAdvanced - Workshop #9

--- CakesAdvanced/Models/Workshop.cs
+++ CakesAdvanced/Models/Workshop.cs
@@ -8,5 +8,28 @@ namespace CakesAdvanced.Models
 {
     internal class Workshop
     {
+        Dictionary<string, Dictionary<string, int>> _recipes = new Dictionary<string, Dictionary<string, int>>();
+        public Dictionary<string, Dictionary<string, int>> GetAllRecipes()
+        {
+            return _recipes;
+        }
+        public Workshop()
+        {
+            Dictionary<string, int> Medovik = new Dictionary<string, int>()
+            {
+                {"Milk",  97},
+                {"Sugar", 120},
+                {"Flour", 100}
+            };
+            _recipes.Add("Medovik", Medovik);
+
+            Dictionary<string, int> Napoleon = new Dictionary<string, int>()
+            {
+                { "Butter",  150},
+                { "Eggs", 440},
+                { "Salt", 75}
+            };
+            _recipes.Add("Napoleon", Napoleon);
+        }
     }
 }
