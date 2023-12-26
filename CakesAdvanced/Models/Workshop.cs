d2a26f849f57772d4cb3f72bfd3b395585b65b8f
Jockey195 <136336256+Jockey195@users.noreply.github.com>
2023-12-26 11:42:12 +0300

    WorkShop#9

--- CakesAdvanced/Models/Workshop.cs
+++ CakesAdvanced/Models/Workshop.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace CakesAdvanced.Models
 {
     internal class Workshop
     {
+        Dictionary<string, Dictionary<string, int>> _recipes = new Dictionary<string, Dictionary<string, int>>();
+        public Dictionary<string, Dictionary<string, int>> GetAllRecipes()
+        {
+            return _recipes;
+        }
+        public Workshop()
+        {
+            Dictionary<string, int> Medovik = new Dictionary<string, int>()
+            {
+                {"Milk",  97},
+                {"Sugar", 120},
+                {"Flour", 100}
+            };
+            _recipes.Add("Medovik", Medovik);
+
+            Dictionary<string, int> Napoleon = new Dictionary<string, int>()
+            {
+                { "Butter",  150},
+                { "Eggs", 440},
+                { "Salt", 75}
+            };
+            _recipes.Add("Napoleon", Napoleon);
+        }
     }
 }
