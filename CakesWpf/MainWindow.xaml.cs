1739ec001f42acd673b9c277a9ddca25f8cd53c4
Movsar Bekaev <movsar.dev@gmail.com>
2024-01-20 11:16:59 +0300

    Commit24

---
+++ CakesWpf/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CakesLibrary.Models;
+using System.Windows;
+
+
+namespace CakesWpf
+{
+    public partial class MainWindow : Window
+    {
+        private Storage _storage;
+        private Kitchen _kitchen;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _storage = new Storage();
+            _kitchen = new Kitchen(_storage);
+        }
+    }
+}
