416f616a3e1b7ba966a8db8031b20789bf766b77
= <=>
2024-01-27 13:06:20 +0300

    с

---
+++ CakesLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CakesLibrary
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CakesLibrary/Models.cs/Kitchen.cs
@@ -0,0 +1,62 @@
+
+using System.Diagnostics;
+
+namespace CakesLibrary.Models
+{
+    public class Kitchen
+    {
+        public event Action<Cake> CakeReady;
+
+        readonly Storage _storage;
+        readonly Workshop _workshop;
+        public Kitchen(Storage storage)
+        {
+            _storage = storage;
+            _workshop = new Workshop();
+        }
+
+        public void MakeCake(string cakeName)
+        {
+            var availableRecipes = GetAvailableRecipes();
+            var recipeForTheCake = availableRecipes.Keys.FirstOrDefault(recipeName => recipeName.ToLower() == cakeName);
+            if (recipeForTheCake == null)
+            {
+                throw new Exception("Нет рецепта для такого чуда :(");
+            }
+
+            var neededIngredients = availableRecipes.First(recipe => recipe.Key.ToLower() == cakeName.ToLower()).Value;
+
+            // Берем необходимые ингредиенты из Склада
+            var ingredients = _storage.TakeIngredients(neededIngredients);
+
+            // Готовим торт
+            Task.Delay(5000);
+            var cake = new Cake(cakeName, ingredients);
+
+            CakeReady?.Invoke(cake);
+        }
+
+        public Dictionary<string, Dictionary<string, int>> GetAvailableRecipes()
+        {
+            var recipes = _workshop.GetAllRecipes();
+            var ingredientsToRecipe = new Dictionary<string, Dictionary<string, int>>();
+
+            foreach (var recipe in recipes)
+            {
+                try
+                {
+                    Debug.WriteLine($"Проверяем можем ли мы приготовить: {recipe.Key}");
+
+                    _storage.VerifyIngredientsAvailability(recipe.Value);
+                    ingredientsToRecipe.Add(recipe.Key, recipe.Value);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+
+            return ingredientsToRecipe;
+        }
+    }
+}
